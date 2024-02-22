2576cdbf7895dcce032150251768e8b19864b260
kosisochukwu25 <kosisochukwuomordi@gmail.com>
2024-02-22 09:48:38 +0100

    feat: create a new file

---
+++ don.js
@@ -0,0 +1,36 @@
+const first=document.getElementById('one')
+
+first.textContent= 'univelcity'
+first.textContent='montgomery'
+
+const val='ol li'
+const third= document.querySelector(val)
+
+const fourth=document.querySelectorAll('ol li')
+
+third.innerText='aramide'
+
+fourth[5].innerText='kosiso'
+fourth[1].innerText='ebade'
+fourth[2].innerText='efosa'
+fourth[3].innerText='adabare'
+
+
+const lists= document.querySelectorAll('ul li')
+lists[6].textContent='yes is a boy'
+lists[5].textContent='we are in the class'
+
+
+const listId=document.querySelectorAll('#one')
+listId[1].textContent='I\'m an id'
+
+const listClass=document.getElementsByClassName('two')
+listClass[1].textContent='aisha'
+
+const qClass= document.querySelectorAll('.two')
+qClass[3].innerHTML='darlington'
+
+
+qClass[2].innerHTML='my name is <strong>kosiso</strong>'
+
+lists[0].innerText='my\n\n\n name is\n kosiso'
---
+++ kosiso.js
@@ -0,0 +1,57 @@
+// // // declaration of variable
+
+// //  let bvn= 1234567890
+// //  bvn= 1213456
+
+// //  let num1
+
+// //  num1=333
+
+
+// const num= 10
+// const num1= num+5
+
+// let num2=num1+5
+
+// let num3=10
+// num3+=5
+// num3*=2
+
+
+let $number=700
+
+let $= 1800
+
+let _=700
+
+let _num5=100
+
+let num6='100'
+
+let kosiso='john'
+
+let myName=kosiso
+
+let office=`school`
+
+let age = 29
+
+let comment='my name is '+myName+'\ni work at office\n\t\t\t'+office
+
+let comment2=`my name is ${myName} and i work at${office}.
+
+i was born in ${new Date().getFullYear()-age}`
+
+let comment3="mohammed Ali says \"suffer now and live the rest of your life a champion\""
+
+let comment4="she'is a girl"
+
+console.log(comment4)
+alert(comment4)
+
+
+
+
+
+
+
